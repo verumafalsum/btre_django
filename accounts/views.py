6c73c364e361736d24da8ebcbbfa5afb6cb23880
verumafalsum <vladimirrudd@gmail.com>
2019-02-06 22:01:17 +0300

    add message alerts

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.views import View
+from django.contrib import messages
 
 class RegisterView(TemplateView):
     template_name = "accounts/register.html"
+
+    def post(self, request):
+        messages.error(request, 'Testing error message')
+        return redirect('register')
 
 
 class LoginView(TemplateView):
     template_name = "accounts/login.html"
 
+    def post(self, request):
+        
+        return redirect('login')
 
 class DashboardView(TemplateView):
     template_name = "accounts/dashboard.html"
--- btre/settings.py
+++ btre/settings.py
@@ -151,3 +151,8 @@ STATICFILES_DIRS = [
 # Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
